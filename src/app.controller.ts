465e2c34177334d1f7f5f1560e09a5949b9dcc73
weblearninghost <lalitavadje14@gmail.com>
2023-09-02 15:12:15 +0530

    api and services add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Param, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/earth-mars-comm/:message')
+  postMessage(@Param('message') message, @Req() req) {
+    const sender = req.headers?.sender;
+    const receiver = req.headers?.receiver;
+    console.log(`Sender:${sender}
+    Receiver:${receiver}
+    `);
+    return this.appService.messageService(message, sender);
   }
 }
